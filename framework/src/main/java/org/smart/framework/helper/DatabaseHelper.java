4ab7ec96873b34e72f4591f3908158c75cbc923e
bohenmian <bohenmian@gmail.com>
2019-08-28 20:41:48 +0800

    feat:compelete database crud method

--- framework/src/main/java/org/smart/framework/helper/DatabaseHelper.java
+++ framework/src/main/java/org/smart/framework/helper/DatabaseHelper.java
@@ -1,5 +1,9 @@
 package org.smart.framework.helper;
 
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.smart.framework.util.PropsUtil;
@@ -7,11 +11,15 @@ import org.smart.framework.util.PropsUtil;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 public class DatabaseHelper {
 
-    public static final Logger LOGGER = LoggerFactory.getLogger(DatabaseHelper.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseHelper.class);
+    private static final QueryRunner QUERY_RUNNER;
 
     private static final String DRIVE;
     private static final String URL;
@@ -20,6 +28,7 @@ public class DatabaseHelper {
 
     static {
         Properties properties = PropsUtil.loadProps("config.properties");
+        QUERY_RUNNER = new QueryRunner();
         DRIVE = properties.getProperty("jdbc.driver");
         URL = properties.getProperty("jdbc.url");
         USERNAME = properties.getProperty("jdbc.username");
@@ -50,5 +59,86 @@ public class DatabaseHelper {
                 LOGGER.error("close connection fail", e);
             }
         }
+    }
+
+    public static <T> T queryEntity(Class<T> entityClass, String sql, Object... params) {
+        T entity;
+        try {
+            Connection connection = getConnection();
+            entity = QUERY_RUNNER.query(connection, sql, new BeanHandler<>(entityClass), params);
+        } catch (SQLException e) {
+            LOGGER.error("query entity failure", e);
+            throw new RuntimeException();
+        }
+        return entity;
+    }
+
+    public static <T> List<T> queryEntityList(Class<T> entityClass, String sql, Object... params) {
+        List<T> entities;
+        try {
+            Connection connection = getConnection();
+            entities = QUERY_RUNNER.query(connection, sql, new BeanListHandler<>(entityClass), params);
+        } catch (SQLException e) {
+            LOGGER.info("query entities failure");
+            throw new RuntimeException();
+        }
+        return entities;
+    }
+
+    public static int update(String sql, Object... params) {
+        int rows;
+        try {
+            Connection connection = getConnection();
+            rows = QUERY_RUNNER.update(connection, sql, params);
+        } catch (SQLException e) {
+            LOGGER.error("execute update failure", e);
+            throw new RuntimeException(e);
+        }
+        return rows;
+    }
+
+    public static <T> boolean insertEntity(Class<T> entityClass, Map<String, Object> fieldMap) {
+        if (MapUtils.isEmpty(fieldMap)) {
+            LOGGER.error("can not insert entity: fieldMap is empty");
+            return false;
+        }
+        String sql = "INSERT INTO " + entityClass.getSimpleName();
+        StringBuilder columns = new StringBuilder("(");
+        StringBuilder values = new StringBuilder("(");
+        for (String fieldName : fieldMap.keySet()) {
+            columns.append(fieldName).append(", ");
+            values.append("?, ");
+        }
+        columns.replace(columns.lastIndexOf(", "), columns.length(), ")");
+        values.replace(values.lastIndexOf(", "), values.length(), ")");
+        sql += columns + " VALUES " + values;
+        Object[] params = fieldMap.values().toArray();
+        return update(sql, params) == 1;
+    }
+
+    public static <T> boolean updateEntity(Class<T> entityClass, long id, Map<String, Object> fieldMap) {
+        if (MapUtils.isEmpty(fieldMap)) {
+            LOGGER.error("can not update entity: fieldMap is empty");
+            return false;
+        }
+
+        String sql = "UPDATE " + entityClass.getSimpleName() + " SET ";
+        StringBuilder columns = new StringBuilder();
+        for (String fieldName : fieldMap.keySet()) {
+            columns.append(fieldName).append(" = ?, ");
+        }
+        sql += columns.substring(0, columns.lastIndexOf(", ")) + " WHERE id = ?";
+
+        List<Object> paramList = new ArrayList<Object>();
+        paramList.addAll(fieldMap.values());
+        paramList.add(id);
+        Object[] params = paramList.toArray();
+
+        return update(sql, params) == 1;
+    }
+
+    public static <T> boolean deleteEntity(Class<T> entityClass, long id) {
+        String sql = "DELETE FROM " + entityClass.getSimpleName() + " WHERE id = ?";
+        return update(sql, id) == 1;
     }
 }
